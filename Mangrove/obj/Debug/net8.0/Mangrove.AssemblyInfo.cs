f6a79004402564218e78703fd765f876303ed284
AnhQuan30052003 <anhquan300503@gmail.com>
2025-02-03 15:04:30 +0700

    cập nhật lại source và chỉnh sửa bố cục từ template

--- Mangrove/obj/Debug/net8.0/Mangrove.AssemblyInfo.cs
+++ Mangrove/obj/Debug/net8.0/Mangrove.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mangrove")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784234ac05789298d4320b0e7f739add935cf502")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d64c2186f558f724b2e04bd572b626ca00e1bcae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mangrove")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mangrove")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
