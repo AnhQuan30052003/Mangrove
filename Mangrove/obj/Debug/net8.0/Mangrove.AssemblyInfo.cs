1ec29ac301947a6503670abb3d478db9e72e9ce0
AnhQuan30052003 <anhquan300503@gmail.com>
2025-02-14 09:58:55 +0700

    sắp hoàn thiện Footer, còn thiếu background-color

--- Mangrove/obj/Debug/net8.0/Mangrove.AssemblyInfo.cs
+++ Mangrove/obj/Debug/net8.0/Mangrove.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mangrove")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17ceda0c56190171ef30c3696cdc927020ac47e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3353022315211bdccb3175ec6a5840eefab5cf50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mangrove")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mangrove")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
