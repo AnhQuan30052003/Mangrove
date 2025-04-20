29fdf26aff1b71482d294ce1d2e76845513aa8c2
AnhQuan30052003 <anhquan300503@gmail.com>
2025-04-20 12:09:01 +0700

    fix show fixed title and icon back some pages

--- Mangrove/obj/Debug/net8.0/Mangrove.AssemblyInfo.cs
+++ Mangrove/obj/Debug/net8.0/Mangrove.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mangrove")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f41303d7d2a15dfa31ee45bdfed891ce81ab5c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+082b562c0b88b669ee803f473c011b45bab1ccad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mangrove")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mangrove")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
