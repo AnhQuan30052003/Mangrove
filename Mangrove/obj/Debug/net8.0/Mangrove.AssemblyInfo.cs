4086a38edc055814de5c9c94197d195f0ade0520
AnhQuan30052003 <anhquan300503@gmail.com>
2025-05-01 21:07:10 +0700

    cập nhật click_show_image cho logo nhà tài trợ và banner trang chủ

--- Mangrove/obj/Debug/net8.0/Mangrove.AssemblyInfo.cs
+++ Mangrove/obj/Debug/net8.0/Mangrove.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mangrove")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e70a904beb5ff1e41bf38d7e79a7dd01be07afc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e60976d85950ebbea1232d584aa6f39f2d3d0d66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mangrove")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mangrove")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
